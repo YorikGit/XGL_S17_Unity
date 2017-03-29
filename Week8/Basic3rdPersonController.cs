4f7d3ea135909d5e374b8638c29b50485567f8be
Sean Han Tani <seagaia@f-m.fm>
2017-03-29 15:17:53 -0500

    week 8 scripts

---
+++ Week8/Basic3rdPersonController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basic3rdPersonController : MonoBehaviour {
+
+	// Use this for initialization
+	Rigidbody rb;
+	public float pushSpeed = 25f;
+	public float jumpSpeed = 30f;
+
+	// Need this to correctly push the player.
+	public Camera cam;
+
+	void Start () {
+		rb = GetComponent <Rigidbody> ();
+	}
+
+	void Update () {
+		
+	}
+
+	// Physics code like applyforge goes in fixedupdate.
+	void FixedUpdate () {
+
+
+		// GetButtonDown is the same as "Just Pressed" (only returns true ONCE when the key is presseD)
+		// GetButton is the same as "Pressed" (returns true AS LONG AS the key is pressed)
+		if (Input.GetButton ("Jump")) {
+			rb.AddForce (0, jumpSpeed, 0);
+		}
+
+		if (Input.GetButton ("Right")) {
+			Vector3 v = Vector3.Normalize (cam.transform.right);
+			v = new Vector3 (v.x, 0, v.z);
+			v *= pushSpeed;
+			rb.AddForce (v);
+		} else if (Input.GetButton ("Left")) {
+			Vector3 v = Vector3.Normalize (cam.transform.right);
+			v = new Vector3 (v.x, 0, v.z);
+			v *= -pushSpeed;
+			rb.AddForce (v);
+		}
+
+		if (Input.GetButton ("Up")) {
+			Vector3 v = Vector3.Normalize (cam.transform.forward);
+			v = new Vector3 (v.x, 0, v.z);
+			v *= pushSpeed;
+			rb.AddForce (v);
+		} else if (Input.GetButton ("Down")) {
+			Vector3 v = Vector3.Normalize (cam.transform.forward);
+			v = new Vector3 (v.x, 0, v.z);
+			v *= -pushSpeed;
+			rb.AddForce (v);
+		}
+
+		if (rb.velocity.y > 5f) {
+			Vector3 v = rb.velocity;
+			v.y = 5f;
+			rb.velocity = v;
+		}
+
+
+		// Movement that works, but not if you rotate the camera view.
+//		if (Input.GetButton ("Up")) {
+//			rb.AddForce (0,0,pushSpeed);	
+//		} else if (Input.GetButton ("Down")) {
+//			rb.AddForce (0,0,-pushSpeed);
+//		}
+
+//		if (Input.GetButton ("Right")) {
+//			rb.AddForce (pushSpeed,0,0);	
+//		} else if (Input.GetButton ("Left")) {
+//			rb.AddForce (-pushSpeed,0,0);
+//		}
+
+
+
+
+
+		// ~example~
+		// GetAxis ("Horizontal") returns a value from -1 to 1, for LEFT held down or RIGHT held down
+//		float hor = Input.GetAxis ("Horizontal");
+//		if (hor > 0) {
+//			// Now the rigidbody is being pushed at a force between 0 and pushspeed, because hor is
+//			// between 0 and 1.
+//			rb.AddForce (hor*pushSpeed,0,0);	
+//		} else if (Input.GetAxis ("Horizontal") < 0) {
+//			rb.AddForce (-pushSpeed,0,0);
+//		}
+
+	}
+
+}
---
+++ Week8/CameraFollowPlayer3rdPerson.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer3rdPerson : MonoBehaviour {
+
+
+	public Transform playerTransform;
+	// How far the camera always is from the player. You can adjust this for a different
+	// initial angle.
+	Vector3 offset;
+	Vector3 targetPosition;
+	float targetYRotation = 0f;
+
+	// Camera's rotation around the player
+	float yRotation = 0f;
+	public float rotateSpeed = 20f;
+
+
+	// Use this for initialization, this code runs once when the attached GameObject is made.
+	void Start () {
+
+		// Hides the cursor
+		Cursor.lockState = CursorLockMode.Locked;
+
+		offset = new Vector3 (0, 4.5f, -14);
+
+		// You can also use this to assign the playerTransform, BUT there must be a gameobject in the
+		// current scene's hierarchy named HoverCube or you will get a null value
+		//playerTransform = GameObject.Find ("HoverCube").GetComponent <Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Gives me a value of how far the mouse has moved since last frame
+		float mouseX = Input.GetAxis ("Mouse X");
+
+		// ~note~ mouse screen axes distances work like 2D games
+		// Positive mouse x movement = negative y rotation.
+
+
+		// Don't rotate unless mouse moving fast enough.
+		if (Mathf.Abs (mouseX) > 0.1f) {
+			// Keep mouseX between -1 and 1 so camera doesnt rotate too fast
+			mouseX = Mathf.Clamp (mouseX,-1,1);
+			targetYRotation += rotateSpeed * mouseX * Time.deltaTime;
+		}
+
+		// Keep rotation values between 0 and 360f
+		if (targetYRotation > 360f) {
+			targetYRotation -= 360f;
+		} else if (targetYRotation < 0f) {
+			targetYRotation += 360f;
+		}
+
+		// Smoothly linerally interpolate the current value of yRotation to targetYRotation.
+		// Remove this for a more instant movement of the camera angle.
+
+		// If the 3rd parameter, t, is 0, then LerpAngle returns yRotation.
+		// If the 3rd parameter is 1, then LerpAngle returns targetYRotation
+		// The formula is (roughly) RETURN_VALUE = t * (targetYRotation - yRotation) + yRotation
+
+		//yRotation = targetYRotation;
+		yRotation = Mathf.LerpAngle(yRotation, targetYRotation, 5f * Time.deltaTime);
+
+		// Multiply this by a vector to rotate that vector by yRotation degrees about the world's y Axis
+		Quaternion qRot = Quaternion.Euler (0, yRotation, 0);
+
+		// Move the camera to the defualt distance from the target
+		transform.position = playerTransform.position + (qRot*offset);
+
+		// Rotate the transform of the camera to view the player
+		transform.LookAt (playerTransform);
+
+	}
+}
